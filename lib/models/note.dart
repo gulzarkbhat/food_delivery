486da581cb63a14d887b38b68bd644b711f2244b
Gulzar Bhat <gulzar@bqe.com>
2019-04-03 23:37:17 +0530

    first commit

---
+++ lib/models/note.dart
@@ -0,0 +1,97 @@
+class Note {
+  int _id;
+  String _title;
+  String _description;
+  String _phone;
+  String _email;
+  String _date;
+  int _priority;
+
+  Note(this._title, this._date, this._priority, this._phone, this._email,
+      [this._description]);
+
+  Note.withId(this._id, this._title, this._date, this._priority, this._phone,
+      this._email,
+      [this._description]);
+
+  int get id => _id;
+
+  String get title => _title;
+
+  String get description => _description;
+
+  int get priority => _priority;
+
+  String get date => _date;
+
+  String get phone => _phone;
+
+  String get email => _email;
+
+  set title(String newTitle) {
+    if (newTitle.length <= 255) {
+      this._title = newTitle;
+    }
+  }
+
+  set description(String newDescription) {
+    if (newDescription.length <= 255) {
+      this._description = newDescription;
+    }
+  }
+
+  set priority(int newPriority) {
+    if (newPriority >= 1 && newPriority <= 2) {
+      this._priority = newPriority;
+    }
+  }
+
+  set date(String newDate) {
+    this._date = newDate;
+  }
+
+  set phone(String contact) {
+    if (contact.length <= 287) this._phone = contact;
+  }
+
+  set email(String email) {
+    if (validateEmail(email)) this._email = email;
+  }
+
+  bool validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return false;
+    else
+      return true;
+  }
+
+  // Convert a Note object into a Map object
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['description'] = _description;
+    map['priority'] = _priority;
+    map['date'] = _date;
+    map['contact'] = _phone;
+    map['email'] = _email;
+
+    return map;
+  }
+
+  // Extract a Note object from a Map object
+  Note.fromMapObject(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._title = map['title'];
+    this._description = map['description'];
+    this._priority = map['priority'];
+    this._date = map['date'];
+    this._phone = map['contact'];
+    this._email = map['email'];
+  }
+}
